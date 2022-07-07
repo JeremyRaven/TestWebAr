de38c4ba2c3033312a93c2e93d1e47082abdb11e
Jeremy Raven <jeremyraven@icloud.com>
2022-07-07 15:10:39 +1200

    initialcommit

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
       const delta = clock.getDelta();
-      //gltf.scene.rotation.set(gltf.scene.rotation.z+delta, 0, 0);
+      gltf.scene.rotation.set(0, gltf.scene.rotation.y+delta, 0, 0);
       renderer.render(scene, camera);
     });
   }
